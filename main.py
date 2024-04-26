68007966487aeec3f68c080ff00e8d47736b8618
bamistand <oluwabamistand@gmail.com>
2024-04-26 15:37:01 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import time
+import math
+from fastapi import FastAPI
+import threading
+
+app = FastAPI()
+
+def generate_cpu_load(interval, utilization):
+    "Generate a utilization % for a duration of interval seconds"
+    while True:  #  loop 
+        start_time = time.time()
+        while time.time() - start_time < interval:
+            print("About to do some arithmetic")
+            end_time = time.time() + (utilization / 100.0)
+            while time.time() < end_time:
+                a = math.sqrt(64 * 64 * 64 * 64 * 64)
+        print("About to sleep")
+        time.sleep(240)  # Pause for 4 minutes
+
+@app.get("/")
+def read_root():
+    threading.Thread(target=generate_cpu_load, args=(30, 100)).start()
+    return {"message": "CPU load generation started"}
+
+
+
+
+
+
+
